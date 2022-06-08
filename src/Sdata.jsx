f710a5fdb3a78dead92f03a40a3cfad1341fe200
tiwariRAFTAAR <tiwariayush445@.com>
2022-06-08 23:50:07 +0530

    second commit

---
+++ src/Sdata.jsx
@@ -0,0 +1,36 @@
+const Sdata = [
+    
+        {
+          imgsrc: "https://www.inspirationde.com/media/2020/07/dark-season-concept-keyart-on-behance-15946476138nk4g.png" ,
+          title: " A Netflix Orignal Series ",
+          sname: " DARK ",
+          link: "https://www.netflix.com/in/title/80100172",
+        },
+        {
+         imgsrc: "https://i.pinimg.com/564x/10/a7/7f/10a77ff158e461ee3ca6752f9feff356.jpg" ,
+          title: " A Netflix Orignal Series ",
+          sname: " Extra Curricular ",
+          link: "https://www.netflix.com/in/title/80990668",
+        },
+        {
+          imgsrc: "https://flxt.tmsimg.com/assets/p12991665_b_v13_am.jpg" ,
+          title: " A Netflix Orignal Series ",
+          sname: " Stranger Things ",
+          link: "https://www.netflix.com/in/title/80057281",
+        },
+        {
+          imgsrc: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRJ9m_3GXswHQIJJmbImSBYk48BdZaIFOM5SuGBpZnd_Wi5vECxtDr5HSgRBgtt3aCyKeQ&usqp=CAU" ,
+          title: " A Netflix Orignal Series ",
+          sname: " Money Heist ",
+          link: "https://www.netflix.com/in/title/80192098",
+        },
+        {
+          imgsrc: "https://upload.wikimedia.org/wikipedia/en/b/b9/Narcos_season_1.png" ,
+          title:" A Netflix Orignal Series ",
+          sname: " Narcos ",
+          link: "https://www.netflix.com/in/title/80025172",
+        },
+        
+];
+
+export default Sdata;
---
+++ src/index.js
@@ -0,0 +1,33 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Card from './Cards';
+import './index.css';
+import Sdata from './Sdata';
+
+
+// function Ncard(val){
+//   return(
+    
+//   );
+// }
+
+
+ReactDOM.render(
+  <>
+
+    <h1 className="heading_style"> List of Top 5 Netflix Series in 2020 </h1>
+
+    {Sdata.map((val) => {
+      return (
+        <Card imgsrc={val.imgsrc}
+        title={val.title}
+        sname={val.sname}
+        link={val.link}
+  />
+      );
+    })}
+    
+  </>,
+    
+  document.getElementById('root')
+);
